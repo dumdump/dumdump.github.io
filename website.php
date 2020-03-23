1c46c2134804a3062aaaddbae06d8b977148bd40
dumdump <61518949+dumdump@users.noreply.github.com>
2020-03-23 18:28:50 -0400

    Update website.php

--- website.php
+++ website.php
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+    <meta charset="UTF-8">
+    <style></style>
+  </head>
+  <body>
+    <?php
+      echo "HELLO!!!!";
+    ?>
+    <script></script>
+  </body>
+</html>
